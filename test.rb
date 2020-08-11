71bc26a12db5bdfa3d45fc31e6f8454dbb96784b
fozia@iuj.ac.jp <fozia@iuj.ac.jp>
2020-08-11 15:36:17 +0000

    Added the htlm page_documentroot

---
+++ test.rb
@@ -0,0 +1,14 @@
+require 'webrick'
+server = WEBrick::HTTPServer.new({
+  :DocumentRoot => '.',
+  :CGIInterpreter => WEBrick::HTTPServlet::CGIHandler::Ruby,
+  :Port => '3000',
+})
+['INT', 'TERM'].each {|signal|
+  Signal.trap(signal){ server.shutdown }
+}
+server.mount('/test', WEBrick::HTTPServlet::ERBHandler, 'foziweb.html.erb')
+server.mount('/indicate.cgi', WEBrick::HTTPServlet::CGIHandler, 'indicate.rb')
+server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya.rb')
+server.mount('/goya.cgi', WEBrick::HTTPServlet::CGIHandler, 'goya3.rb')
+server.start
